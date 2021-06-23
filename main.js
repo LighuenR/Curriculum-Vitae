97da46d25afd943df69801ad886d85acba4fd869
LighuenR <lighuen.restanio@gmail.com>
2021-06-23 13:23:46 -0300

    Cv html bootstrap js

---
+++ main.js
@@ -0,0 +1,105 @@
+const nombre = "Lighuen Restanio"
+
+const experiencia = [
+    {
+        lugar: "La Table de Xavier MATHIEU",
+        periodo: "Sep 2018 - Dic 2020",
+        descripción: "Chef de partida estacion pescados, encargado de pasantes, manipulacion de materia prima"
+    },
+    {
+    lugar: "Republica restaurant",
+    periodo: "Ene 2016 -  Dic 2018",
+    descripción: "Encargado de cocina, responsable de la produccion con personal a cargo"
+    }
+    
+    
+];
+
+const estudios = [{
+    lugar: "IGA Córdoba | Instituto Gastronómico de las Américas",
+    periodo: "2013-2015",
+    descripción: "Profesional Gastronomico"
+    },
+    {
+    lugar: "Saul A. Taborda",
+    periodo: "2004-2009",
+    descripción: "Bachiller en Ciencias Naturales y Medio Ambiente"
+    }
+    
+];
+
+/* const lenguajesActuales = [
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bitbucket/bitbucket-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/slack/slack-original.svg",
+    "https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-plain.svg",
+] */
+
+const lenguajesAprendiendo = [
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"    
+]
+
+// Cambia el nombre del H1 que con tiene el nombre del usuario
+var nombreDOM = document.getElementById("name")
+nombreDOM.innerText = nombre
+
+// Itineración que crea los items en "Experiencia"
+for (i = 0; i < experiencia.length; i++){
+    var item = document.createElement("div")
+    item.innerHTML =
+    `<div class="d-flex justify-content-between mt-3">
+    <span class="card-title h6 exp-title">${experiencia[i].lugar}</span>
+    <span class="exp-date">${experiencia[i].periodo}</span>
+    </div>
+    <p>${experiencia[i].descripción}</p>`
+    document.getElementById("experiencia").appendChild(item)
+}
+
+// Itineración que crea los items en "Estudios"
+for (i = 0; i < estudios.length; i++){
+    var item = document.createElement("div")
+    item.innerHTML =
+    `<div class="d-flex justify-content-between">
+    <span class="card-title h6 exp-title">${estudios[i].lugar}</span>
+    <span class="exp-date">${estudios[i].periodo}</span>
+    </div>
+    <p>${estudios[i].descripción}</p>`
+    document.getElementById("estudios").appendChild(item)
+}
+
+// Itineración que crea los items en "Lenguajes y programas"
+/* for (i = 0; i < lenguajesActuales.length; i++){
+    var item = document.createElement("div")
+    item.innerHTML = `<img class="languages" align="left" alt="JavaScript" width="40" src="${lenguajesActuales[i]}" />`
+    document.getElementById("lenguajes_actuales").appendChild(item)
+} */
+
+// Itineración que crea los items en "Lenguajes y tecnologias aprendiendo"
+for (i = 0; i < lenguajesAprendiendo.length; i++){
+    var item = document.createElement("div")
+    item.innerHTML = `<img class="languages" align="left" alt="JavaScript" width="40" src="${lenguajesAprendiendo[i]}" />`
+    document.getElementById("lenguajes_aprendiendo").appendChild(item)
+}
+
+
+function printDiv(printable){
+    var printContents = document.getElementById(printable).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
